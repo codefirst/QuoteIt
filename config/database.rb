03d970c113591fbd9d55a5e7cf3976ce15f610dc
mzp <mzpppp@gmail.com>
2013-12-22 12:18:11 +0000

    clear all

--- config/database.rb
+++
@@ -1,9 +0,0 @@
-Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    config.master = conn.db(uri.path.gsub(/^\//, ''))
-  else
-    config.master = Mongo::Connection.new('localhost',27017).db('quoteit')
-  end
-end
--- config/wedata.rb
+++
@@ -1,2 +0,0 @@
-WEDATA_THUMBNAIL = 'http://wedata.net/databases/QuoteIt-thumbnail/items'
-WEDATA_CLIP      = 'http://wedata.net/databases/QuoteIt-clip/items'
