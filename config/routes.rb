d4c687414206e09e7cf92c65b7e39e237276b7d3
mzp <mzpppp@gmail.com>
2013-12-22 04:05:30 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Quoteit::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'top#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
