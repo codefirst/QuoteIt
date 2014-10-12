e44d559169154800b58726a875d4af48f6e5daec
mallowlabs <mallowlabs@gmail.com>
2014-10-12 13:10:02 +0900

    Added: catch OpenURI::HTTPError refs #20

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -2,18 +2,54 @@ require 'spec_helper'
 
 describe QuotesController do
   context 'show' do
-    before { get :show, :u => nil }
-    subject { response }
-    its(:response_code) { should eq 404 }
+    context 'with nil' do
+      before { get :show, :u => nil }
+      subject { response }
+      its(:response_code) { should eq 404 }
+    end
+    context 'with invalid' do
+      before { get :show, :u => 'http://' }
+      subject { response }
+      its(:response_code) { should eq 404 }
+    end
+    context 'with blacklisted url' do
+      before { get :show, :u => 'http://localhost/' }
+      subject { response }
+      its(:response_code) { should eq 404 }
+    end
   end
-  context 'show' do
-    before { get :show, :u => 'http://' }
-    subject { response }
-    its(:response_code) { should eq 404 }
-  end
-  context 'show with blacklisted url' do
-    before { get :show, :u => 'http://localhost/' }
-    subject { response }
-    its(:response_code) { should eq 404 }
+  context 'html' do
+    context 'with nil' do
+      before { get :html, :format => 'json', :u => nil }
+      subject { response }
+      its(:response_code) { should eq 404 }
+    end
+    context 'with HTTPError 404' do
+      before {
+        err = OpenURI::HTTPError.new('404 Not Found', nil)
+        HtmlRule.stub(:quote).and_raise(err)
+        get :html, :format => 'json', :u => 'http://www.example.com'
+      }
+      subject { response }
+      its(:response_code) { should eq 404 }
+    end
+    context 'with HTTPError 403' do
+      before {
+        err = OpenURI::HTTPError.new('403 Forbidden', nil)
+        HtmlRule.stub(:quote).and_raise(err)
+        get :html, :format => 'json', :u => 'http://www.example.com'
+      }
+      subject { response }
+      its(:response_code) { should eq 403 }
+    end
+    context 'with HTTPError 503' do
+      before {
+        err = ::OpenURI::HTTPError.new('503 Service Unavailable', nil)
+        HtmlRule.stub(:quote).and_raise(err)
+        get :html, :format => 'json', :u => 'http://www.example.com'
+      }
+      subject { response }
+      its(:response_code) { should eq 503 }
+    end
   end
 end
